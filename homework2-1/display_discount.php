463037101f2cc16053683a346dbb8ed4a2c97eef
ankurgyani <arg58@njit.edu>
2017-02-11 04:22:08 -0500

    first change made into the php file

--- homework2-1/display_discount.php
+++ homework2-1/display_discount.php
@@ -1,4 +1,12 @@
+<?php>
+$product_discription = $_POST['product_discription'];
+$list_price = $_POST['list_price'];
+$discount_percent= $_POST['discount_percent'];
+
+?>
+
 <!DOCTYPE html>
+>
 <html>
 <head>
     <title>Product Discount Calculator</title>
@@ -9,13 +17,14 @@
         <h1>This page is under construction</h1>
 
         <label>Product Description:</label>
-        <span><?php echo ''; ?></span><br>
+        
+	<span><?php echo $product_description; ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $list_price; ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_percent; ?></span><br>
 
         <label>Discount Amount:</label>
         <span><?php echo ''; ?></span><br>
