ccd596cd2e47853cb8f777342b16cbb48abb1009
ankurgyani <arg58@njit.edu>
2017-02-13 03:31:47 -0500

    investment rate updated

--- homework2-2/display_results.php
+++ homework2-2/display_results.php
@@ -63,16 +63,17 @@
         <h1>Future Value Calculator</h1>
 
         <label>Investment Amount:</label>
-        <span><?php echo $investment_f; ?></span><br>
+        <span><?php echo htmlspecialchars( $investment_f); ?></span><br/>
 
         <label>Yearly Interest Rate:</label>
-        <span><?php echo $yearly_rate_f; ?></span><br>
+        <span><?php echo htmlspecialchars( $yearly_rate_f); ?></span><br/>
 
         <label>Number of Years:</label>
-        <span><?php echo $years; ?></span><br>
+        <span><?php echo htmlspecialchars( $years); ?></span><br/>
 
         <label>Future Value:</label>
-        <span><?php echo $future_value_f; ?></span><br>
+        <span><?php echo htmlspecialchars( $future_value_f); ?></span><br/>
     </main>
 </body>
-</html>
+</html
+>
